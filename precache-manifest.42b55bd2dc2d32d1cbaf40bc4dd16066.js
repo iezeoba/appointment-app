599c244f6e5ddd82da46f2d977f2dc20e819195d
Iynaefi <47215909+Iynaefi@users.noreply.github.com>
2020-09-20 20:38:17 -0600

    Updates

---
+++ precache-manifest.42b55bd2dc2d32d1cbaf40bc4dd16066.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe86d646d9d1b9f06da55ca0c55479d5",
+    "url": "/appointment-app/index.html"
+  },
+  {
+    "revision": "0a222ac1b330ce1f6169",
+    "url": "/appointment-app/static/css/2.f3cffc9e.chunk.css"
+  },
+  {
+    "revision": "1eff09bcf7e35f256d51",
+    "url": "/appointment-app/static/css/main.354cf76c.chunk.css"
+  },
+  {
+    "revision": "0a222ac1b330ce1f6169",
+    "url": "/appointment-app/static/js/2.3f829cc3.chunk.js"
+  },
+  {
+    "revision": "acb364211461e18f66aec1e8c68e542c",
+    "url": "/appointment-app/static/js/2.3f829cc3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1eff09bcf7e35f256d51",
+    "url": "/appointment-app/static/js/main.713b44d8.chunk.js"
+  },
+  {
+    "revision": "3ed0551f183b035ee3d4",
+    "url": "/appointment-app/static/js/runtime-main.aa98ee13.js"
+  }
+]);
